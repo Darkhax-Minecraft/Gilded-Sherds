c681f377935a2da639dfff1ba3a4b3f9b184c05d
Tyler Hancock <darklime@live.ca>
2024-01-22 15:28:54 -0700

    Port to MC 1.20.2

---
+++ neoforge/src/main/java/net/darkhax/gildedsherds/GildedSherdsNeoforge.java
@@ -0,0 +1,19 @@
+package net.darkhax.gildedsherds;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.registries.RegisterEvent;
+
+@Mod(GildedSherdsCommon.MOD_ID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class GildedSherdsNeoforge {
+
+    @SubscribeEvent
+    public static void registerStuff(RegisterEvent event) {
+
+        event.register(Registries.ITEM, helper -> GildedSherdsCommon.registerItems((id, item) -> helper.register(id, item)));
+        event.register(Registries.CREATIVE_MODE_TAB, helper -> helper.register(GildedSherdsCommon.TAB_ID, GildedSherdsCommon.createTab(CreativeModeTab.builder())));
+    }
+}
